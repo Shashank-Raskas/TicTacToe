e555059548a6c09ab0d4475d9f46fa3c85d92d92
Shashank-Raskas <shashankrk20@gmail.com>
2025-01-24 22:24:12 +0530

    computing values and avoiding unneccesary state mgmt

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,68 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 import { useState } from "react";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X')
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {    {/*getting the state of the game turns*/}
+      let currentPlayer = 'X';
+      
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {   //if the first player is X then the next player is O
+        currentPlayer = 'O';  
+      }
+
+      const updateTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, // currrent turn data
+      ...prevTurns,  //spread operator to get the all the previous turns, prevTurns contains all the moves made so far in the game (from previous turns).
+      ];
+      return updateTurns;
+    });
   }
+
+  // function Parent() {
+  //   const [count, setCount] = useState(0);
+
+  //   return (
+  //     <div>
+  //       <ChildA count={count} setCount={setCount} />
+  //       <ChildB count={count} setCount={setCount} />  //!this is called lifting up the state where if two components can use a common state in the other component
+  //     </div>
+  //   );
+  // }
+
+  // function ChildA({ count, setCount }) {
+  //   return (
+  //     <div>
+  //       <p>Child A: {count}</p>
+  //       <button onClick={() => setCount(count + 1)}>Increment A</button>
+  //     </div>
+  //   );
+  // }
+
+  // function ChildB({ count, setCount }) {
+  //   return (
+  //     <div>
+  //       <p>Child B: {count}</p>
+  //       <button onClick={() => setCount(count + 1)}>Increment B</button>
+  //     </div>
+  //   );
+  // }
+
   return (
     <main>
       <div id="game-container">
         <ol id='players' className="highlight-player">
-          <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
-          <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'}/>
+          <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
+          <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>  {/*lifting up the state where if two components can use a common state in the other component*/}
+        {/* <Parent /> */}
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />  {/*lifting up the state where if two components can use a common state in the other component*/}
       </div>
+      <Log />
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,18 +5,18 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-    const [gameboard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({onSelectSquare}) {
+    // const [gameboard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard =[...prevGameBoard.map(innerArray => [...innerArray])];   // changing values with reference as it objects
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;    //updating it in a immutable way
-            return updatedBoard;
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard =[...prevGameBoard.map(innerArray => [...innerArray])];   // changing values with reference as it objects
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;    //updating it in a immutable way
+    //         return updatedBoard;
 
-        });
-        onSelectSquare();
-    }
+    //     });
+    //     onSelectSquare();
+    // }
     return (
         <ol id='game-board'>
             {gameboard.map((row, rowIndex) => (
@@ -25,7 +24,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => {handleSelectSquare(rowIndex,colIndex)}}>{playerSymbol}</button>
+                                <button onClick={onSelectSquare}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
