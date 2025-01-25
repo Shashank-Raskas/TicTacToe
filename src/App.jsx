80d40ec104efb9e763d2360d5a658a01d34ac846
Shashank-Raskas <shashankrk20@gmail.com>
2025-01-25 13:34:20 +0530

    minimizing the states use and craeted function for player switching and reusing it

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
     <main>
       <div id="game-container">
         <ol id='players' className="highlight-player">
-          <Player initialName='Player 1' symbol='X' isActive={gameTurns.player === 'X'} />
+          <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
           <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
         {/* <Parent /> */}
